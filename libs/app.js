ef378a6350a0cf98339dd8ef37cf1636323778bd
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 01:00:27 +0200

    test firebase

--- libs/app.js
+++ libs/app.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-  var data = document.querySelector('a-scene').systems.firebase.firebase;
-
-  console.log("data : "+data);
+  $('#Start').on('click',function(){
+    document.querySelector('#Start').emit('fade');
+    var entities = [].slice.call(document.querySelectorAll("#Start"));
+    entities.forEach(function (el) { el.emit('start'); });
+    console.log("je suis cliqué");
+  });
 });

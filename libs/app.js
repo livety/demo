b81b7cc0f8bf21bbe84b21d77fa9997b2291b365
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 01:49:10 +0200

    console log data firebase

--- libs/app.js
+++ libs/app.js
@@ -4,6 +4,5 @@ $(document).ready(function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
     console.log(data);
-    );
   });
 });

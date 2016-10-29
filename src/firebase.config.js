50b71f4c5ced5c2f65ff4f4ece5052df45830156
Pohaki <kevin.lachaise@gmail.com>
2016-10-29 11:33:35 +0200

    first commit with firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyCBA0iHJ5TvKBLpOSlXnHWwgsp-HIdLBBA",
+  authDomain: "livety-693e4.firebaseapp.com",
+  databaseURL: "https://livety-693e4.firebaseio.com",
+  storageBucket: "<BUCKET>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);

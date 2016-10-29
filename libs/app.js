9a40a111edd203566bb5ec7d5c7c49d2d85843dd
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 01:09:01 +0200

    add animation

--- libs/app.js
+++ libs/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $(document).on('click',function(){
+  $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
     console.log("je suis cliqué");

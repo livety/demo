8f034b9084da4834246703b0e0dcba670b1ef186
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 01:03:30 +0200

    add cursor

--- libs/app.js
+++ libs/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   $('#Start').on('click',function(){
-    document.querySelector('#Start').emit('fade');
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
     console.log("je suis cliqué");

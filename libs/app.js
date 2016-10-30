bd144aa6317e383bca3730927606056865ff90c8
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 02:12:04 +0100

    test

--- libs/app.js
+++ libs/app.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
-  var data = AFRAME.utils.entity.getComponentProperty;
-  var positionCube = data('#cube','firebase-broadcast','#cube');
+  var data = document.querySelector('a-scene').systems.firebase.firebase;
   $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
-    console.log(positionCube);
+    console.log(data);
   });
 });

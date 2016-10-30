419815750f7f822649bd4551b75ea63c47671abe
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 02:57:05 +0200

    reset

--- libs/app.js
+++ libs/app.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
   var data = AFRAME.utils.entity.getComponentProperty;
+  var positionCube = data('#cube','position',components['position']);
   $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
-    console.log(data);
+    console.log(positionCube);
   });
 });

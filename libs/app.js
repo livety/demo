7a00a36549e22023e2358127cdfa6beae27d51cd
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 02:04:23 +0100

    test getComponentProperty

--- libs/app.js
+++ libs/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var data = AFRAME.utils.entity.getComponentProperty;
-  var positionCube = data('#cube');
+  var positionCube = data('#cube','firebase-broadcast');
   $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });

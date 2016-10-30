27ccd7310279375fc51ee49a9148676de3084cfe
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 03:43:05 +0100

    add var for firebase database

--- libs/app.js
+++ libs/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var database = firebase.database().ref('demo/');
+  var database = firebase.database();
   $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });

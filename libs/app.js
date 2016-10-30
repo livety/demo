319257f41b22ab1107afb0504a43b4bc9b2e98b8
Pohaki <kevin.lachaise@gmail.com>
2016-10-30 02:37:08 +0200

    reset

--- libs/app.js
+++ libs/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  var data = document.querySelector('a-scene').systems.firebase.firebase;
+  var data = document.querySelector('a-scene').systems.firebase.data;
   $('#Start').on('click',function(){
     var entities = [].slice.call(document.querySelectorAll("#Start"));
     entities.forEach(function (el) { el.emit('start'); });
